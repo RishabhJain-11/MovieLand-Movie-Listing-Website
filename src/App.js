3e1f9e18ca51dc79ea46bce197bc187a993e8972
Rishabh Jain <jainrishabh71011@gmail.com>
2022-05-09 19:13:49 +0530

    changed on 9th May 2022

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+//Here is your key: 73f7e538
 
+const API_URL = 'http://www.omdbapi.com?apikey=73f7e538';
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+    useEffect(() => {
+        searchMovies('Spiderman')
+    }, []);
     return (
         <h1>
             App
